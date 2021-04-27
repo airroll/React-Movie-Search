3fbd0bfbc18d63c1d2fc8706278e5b52bf13337e
airroll <chnleonard@hotmail.com>
2021-04-27 20:39:51 +0800

    Update searchMovies.js

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -34,7 +34,7 @@ export default function SearchMovies() {
                     value={query}
                     className="input"
                     onChange={(e) => setQuery(e.target.value)}
-                    placeholder="i.e. Jurassic Park"
+                    placeholder="i.e. Jurassic Park Testing 123 Cloud Cloud"
                 />
                 <button className="button" type="submit">
                     Search!
